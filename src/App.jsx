167d05632632b63dedf8b489f45244707a0d68e3
Sunny Verma <161021054+sunnyV0207@users.noreply.github.com>
2025-07-15 12:29:47 +0530

    Added new feature or fixed bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import './App.css'
-import Navbar from './components/navbar/navbar'
-import Home from './components/home/home'
+import Navbar from './components/navbar/navbar.jsx'
+import Home from './components/home/home.jsx'
+import About from './components/about/about.jsx'
 
 function App() {
   
